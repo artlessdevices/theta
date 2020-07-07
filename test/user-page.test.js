28a8058b6a6eda1e74aa9aabc58b084bb698d26f
Kyle E. Mitchell <kyle@kemitchell.com>
2020-07-07 16:14:15 -0700

    Refactor user page tests

--- test/user-page.test.js
+++ test/user-page.test.js
@@ -21,37 +21,34 @@ tape('user page', test => {
   const password = 'ana password'
   const email = 'ana@example.com'
   server((port, done) => {
-    let browser
-    webdriver()
-      .then(loaded => { browser = loaded })
-      .then(() => {
-        return new Promise((resolve, reject) => signup({
-          browser, port, name, location, handle, password, email
-        }, error => {
-          if (error) reject(error)
-          resolve()
-        }))
-      })
+    (async () => {
+      const browser = await webdriver()
+      await new Promise((resolve, reject) => signup({
+        browser, port, name, location, handle, password, email
+      }, error => {
+        if (error) reject(error)
+        resolve()
+      }))
+
       // Browse user page.
-      .then(() => browser.navigateTo(`http://localhost:${port}/~${handle}`))
-      .then(() => browser.$('h2'))
-      .then(h2 => h2.getText())
-      .then(text => test.equal(text, handle, 'handle'))
+      await browser.navigateTo(`http://localhost:${port}/~${handle}`)
+      const h2 = await browser.$('h2')
+      const h2Text = await h2.getText()
+      test.equal(h2Text, handle, 'handle')
+
       // Create project.
-      .then(() => login({ browser, port, handle, password }))
-      .then(() => createProject({ browser, port, project, url, price, category }))
+      await login({ browser, port, handle, password })
+      await createProject({ browser, port, project, url, price, category })
+
       // Find project link on user page.
-      .then(() => browser.navigateTo(`http://localhost:${port}/~${handle}`))
-      .then(() => browser.$('.projects'))
-      .then(projects => projects.$(`=${project}`))
-      .then(link => link.waitForExist())
-      .then(() => browser.saveScreenshot('../test.png'))
-      .then(() => test.pass('project link on user page'))
-      .then(() => finish())
-      .catch(error => {
-        test.fail(error, 'catch')
-        finish()
-      })
+      await browser.navigateTo(`http://localhost:${port}/~${handle}`)
+      const projects = await browser.$('.projects')
+      const link = await projects.$(`=${project}`)
+      await link.waitForExist()
+      test.pass('project link on user page')
+    })()
+      .then(finish)
+      .catch(finish)
     function finish () {
       test.end()
       done()
@@ -75,107 +72,112 @@ tape('user page licenses', test => {
     email: 'bob@example.com'
   }
   server((port, done) => {
-    let browser, cardNumber
-    webdriver()
-      .then(loaded => { browser = loaded })
-      .then(() => {
-        return new Promise((resolve, reject) => signup(
-          Object.assign({}, ana, { browser, port }),
-          error => {
-            if (error) reject(error)
-            resolve()
-          }
-        ))
-      })
-      .then(() => login({ browser, port, handle: ana.handle, password: ana.password }))
-      // Connect.
-      .then(() => browser.$('#account'))
-      .then(account => account.click())
-      .then(() => browser.$('#connect'))
-      .then(connect => connect.click())
-      .then(() => browser.$('=Skip this account form'))
-      .then((element) => element.click())
-      // Confirm connected.
-      .then(() => browser.$('#disconnect'))
-      .then(disconnect => disconnect.getText())
-      .then(text => test.equal(text, 'Disconnect Stripe Account', 'connected'))
-      // Create project.
-      .then(() => createProject({
-        browser, port, project, url, price, category
-      }))
-      .then(() => logout({ browser, port }))
-      // As Bob...
-      .then(() => {
-        return new Promise((resolve, reject) => signup(
-          Object.assign({}, bob, { browser, port }),
-          error => {
-            if (error) reject(error)
-            resolve()
-          }
-        ))
-      })
-      .then(() => login({ browser, port, handle: bob.handle, password: bob.password }))
-      // Buy a license.
-      .then(() => browser.navigateTo(`http://localhost:${port}/~${ana.handle}/${project}`))
-      // Confirm customer details are already prefilled.
-      .then(() => browser.$('#buyForm input[name=name]'))
-      .then(name => name.getValue())
-      .then(value => test.equal(value, bob.name, 'prefilled name'))
-      .then(() => browser.$('#buyForm input[name=email]'))
-      .then(email => email.getValue())
-      .then(value => test.equal(value, bob.email, 'prefilled e-mail'))
-      .then(() => browser.$('#buyForm input[name=location]'))
-      .then(location => location.getValue())
-      .then(value => test.equal(value, bob.location, 'prefilled location'))
-      // Enter credit card information.
-      .then(() => browser.$('iframe'))
-      .then((frame) => browser.switchToFrame(frame))
-      .then(() => browser.$('input[name="cardnumber"]'))
-      .then((input) => { cardNumber = input })
-      .then(() => cardNumber.addValue('42'))
-      .then(() => timeout(200))
-      .then(() => cardNumber.addValue('42'))
-      .then(() => timeout(200))
-      .then(() => cardNumber.addValue('42'))
-      .then(() => timeout(200))
-      .then(() => cardNumber.addValue('42'))
-      .then(() => timeout(200))
-      .then(() => cardNumber.addValue('42'))
-      .then(() => timeout(200))
-      .then(() => cardNumber.addValue('42'))
-      .then(() => timeout(200))
-      .then(() => cardNumber.addValue('42'))
-      .then(() => timeout(200))
-      .then(() => cardNumber.addValue('42'))
-      .then(() => browser.$('input[name="exp-date"]'))
-      .then((input) => input.setValue('10 / 31'))
-      .then(() => browser.$('input[name="cvc"]'))
-      .then((input) => input.setValue('123'))
-      .then(() => browser.$('input[name="postal"]'))
-      .then((input) => input.setValue('12345'))
-      .then(() => browser.switchToParentFrame())
-      // Accept terms.
-      .then(() => browser.$('#buyForm input[name=terms]'))
-      .then(terms => terms.click())
-      .then(() => browser.$('#buyForm button[type=submit]'))
-      .then(submit => submit.click())
-      .then(() => browser.$('.message'))
-      .then(message => message.waitForExist({ timeout: 10000 }))
-      .then(() => browser.$('.message'))
-      .then(message => message.getText())
-      .then(text => test.assert(text.includes('Thank you', 'confirmation')))
-      .then(() => timeout(5000))
-      // Browse to Bob's user page.
-      .then(() => browser.navigateTo(`http://localhost:${port}/~${bob.handle}`))
-      .then(() => browser.$('main .licenses a'))
-      .then(a => a.getAttribute('href'))
-      .then(href => test.equal(href, `/~${ana.handle}/${project}`))
-      .then(finish)
-      .catch(error => {
-        test.fail(error, 'catch')
-        finish()
-      })
+    (async () => {
+      const browser = await webdriver()
+      await new Promise((resolve, reject) => signup(
+        Object.assign({}, ana, { browser, port }),
+        error => {
+          if (error) reject(error)
+          resolve()
+        }
+      ))
+      await login({ browser, port, handle: ana.handle, password: ana.password })
 
+      // Connect.
+      const accountLink = await browser.$('#account')
+      await accountLink.click()
+      const connectLink = await browser.$('#connect')
+      await connectLink.click()
+      const skipLink = await browser.$('=Skip this account form')
+      await skipLink.click()
+
+      // Confirm connected.
+      const disconnect = await browser.$('#disconnect')
+      await disconnect.waitForExist()
+
+      // Create project.
+      await createProject({ browser, port, project, url, price, category })
+      await logout({ browser, port })
+
+      // As Bob...
+      await new Promise((resolve, reject) => signup(
+        Object.assign({}, bob, { browser, port }),
+        error => {
+          if (error) reject(error)
+          resolve()
+        }
+      ))
+      await login({ browser, port, handle: bob.handle, password: bob.password })
+
+      // Buy a license.
+      await browser.navigateTo(`http://localhost:${port}/~${ana.handle}/${project}`)
+
+      // Confirm customer details are already prefilled.
+      const nameInput = await browser.$('#buyForm input[name=name]')
+      const nameValue = await nameInput.getValue()
+      test.equal(nameValue, bob.name, 'prefilled name')
+
+      const emailInput = await browser.$('#buyForm input[name=email]')
+      const emailValue = await emailInput.getValue()
+      test.equal(emailValue, bob.email, 'prefilled e-mail')
+
+      const locationInput = await browser.$('#buyForm input[name=location]')
+      const locationValue = await locationInput.getValue()
+      test.equal(locationValue, bob.location, 'prefilled location')
+
+      // Enter credit card information.
+      const iframe = await browser.$('iframe')
+      await browser.switchToFrame(iframe)
+      const card = await browser.$('input[name="cardnumber"]')
+      await card.addValue('42')
+      await timeout(200)
+      await card.addValue('42')
+      await timeout(200)
+      await card.addValue('42')
+      await timeout(200)
+      await card.addValue('42')
+      await timeout(200)
+      await card.addValue('42')
+      await timeout(200)
+      await card.addValue('42')
+      await timeout(200)
+      await card.addValue('42')
+      await timeout(200)
+      await card.addValue('42')
+      await timeout(200)
+
+      const expirationInput = await browser.$('input[name="exp-date"]')
+      await expirationInput.setValue('10 / 31')
+
+      const cvcInput = await browser.$('input[name="cvc"]')
+      await cvcInput.setValue('123')
+
+      const postalInput = await browser.$('input[name="postal"]')
+      await postalInput.setValue('12345')
+
+      await browser.switchToParentFrame()
+
+      // Accept terms.
+      const termsBox = await browser.$('#buyForm input[name=terms]')
+      await termsBox.click()
+
+      const submitButton = await browser.$('#buyForm button[type=submit]')
+      await submitButton.click()
+
+      const message = await browser.$('.message')
+      await message.waitForExist({ timeout: 10000 })
+      const messageText = await message.getText()
+      test.assert(messageText.includes('Thank you', 'confirmation'))
+      await timeout(5000)
+
+      // Browse to Bob's user page.
+      await browser.navigateTo(`http://localhost:${port}/~${bob.handle}`)
+      const anchor = await browser.$('main .licenses a')
+      const href = await anchor.getAttribute('href')
+      test.equal(href, `/~${ana.handle}/${project}`)
+    })()
+      .then(finish)
+      .catch(finish)
     function finish () {
       test.end()
       done()
@@ -190,29 +192,27 @@ tape('user JSON', test => {
   const password = 'ana password'
   const email = 'ana@example.com'
   server((port, done) => {
-    let browser
-    webdriver()
-      .then(loaded => { browser = loaded })
-      .then(() => {
-        return new Promise((resolve, reject) => signup({
-          browser, port, name, location, handle, password, email
-        }, error => {
-          if (error) reject(error)
-          resolve()
-        }))
-      })
+    (async () => {
+      const browser = await webdriver()
+      await new Promise((resolve, reject) => signup({
+        browser, port, name, location, handle, password, email
+      }, error => {
+        if (error) reject(error)
+        resolve()
+      }))
       // Create project.
-      .then(() => login({ browser, port, handle, password }))
-      .then(() => browser.$('=Account'))
-      .then(account => account.click())
-      .then(() => browser.$('=Create Project'))
-      .then(create => create.click())
-      .then(() => browser.$('#createForm input[name="project"]'))
-      .then(input => input.addValue(project))
-      .then(() => browser.$('#createForm input[name="url"]'))
-      .then(input => input.addValue('http://example.com'))
-      .then(() => browser.$('#createForm button[type="submit"]'))
-      .then(submit => submit.click())
+      await login({ browser, port, handle, password })
+      const accountLink = await browser.$('=Account')
+      await accountLink.click()
+      const createInput = await browser.$('=Create Project')
+      await createInput.click()
+      const projectInput = await browser.$('#createForm input[name="project"]')
+      await projectInput.addValue(project)
+      const urlInput = await browser.$('#createForm input[name="url"]')
+      await urlInput.addValue('http://example.com')
+      const submitButton = await browser.$('#createForm button[type="submit"]')
+      await submitButton.click()
+    })()
       .then(() => {
         http.request({
           port,
@@ -232,10 +232,7 @@ tape('user JSON', test => {
           })
           .end()
       })
-      .catch(error => {
-        test.fail(error, 'catch')
-        finish()
-      })
+      .catch(finish)
     function finish () {
       test.end()
       done()
